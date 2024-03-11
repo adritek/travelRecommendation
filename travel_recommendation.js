33b8319fabe8aeb25ebf1d21f5bef47483a14ce8
Adrian Rogers <adritek@gmail.com>
2024-03-11 11:43:18 -0400

    working on recommendations

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,1 +1,36 @@
-console.log("hello")
+const recommendationsAPI = './travel_recommendation_api.json';
+
+let keywordSubmit = document.getElementById('searchBtn');
+let keywordSearch = document.getElementById("searchQuery").value;
+
+keywordSubmit.addEventListener('click', returnKeywordSearch);
+
+
+function returnKeywordSearch(){
+    let keyword = keywordSearch.toLowerCase();
+    console.log(`Keyword search: ${keyword}`)
+    if (keyword === 'beach' || 'beaches'){
+        fetchRecommendations(beaches);
+        resetSearch();
+    }
+}
+
+function fetchRecommendations() {
+    fetch(recommendationsAPI)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.beaches);
+        // const recommendation = data.conditions.find(item => item.name.toLowerCase() === input);
+        
+        // Further processing of the data can be done here
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    }); 
+}
+
+fetchRecommendations()
+function resetSearch(){
+    document.getElementById("searchQuery").value = "";
+}
