11920002a56a518de0ea15c1984efe5d6300ca55
Adrian Rogers <adritek@gmail.com>
2024-03-12 10:34:58 -0400

    working recommendations

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,11 @@
 const recommendationsAPI = './travel_recommendation_api.json';
 
-// fetch the details from the API based on the keyword the user enters: beach, temple, or country.
-
 let keywordSubmit = document.getElementById('searchBtn');
-let cb = document.getElementById('clearBtn');
+let clearResults = document.getElementById('clearBtn');
+let resultsDiv = document.getElementById("results");
+
 keywordSubmit.addEventListener('click', returnKeywordSearch);
-cb.addEventListener('click', resetSearch);
+clearResults.addEventListener('click', clearDestinations);
 
 function returnKeywordSearch() {
     let keywordSearch = document.getElementById("searchQuery").value;
@@ -41,23 +41,23 @@ function fetchRecommendations(term) {
                 const btn = document.createElement("button");
 
                 if (term === 'countries' || term === 'country') {
+                    title.textContent = item.cities[0].name;
                     img.setAttribute("src", item.cities[0].imageUrl);
                     paragraph.textContent = item.cities[0].description;
+                } else {
+                    img.setAttribute("src", item.imageUrl);
+                    img.setAttribute("width", "500px");
+                    title.textContent = item.name;
+                    paragraph.textContent = item.description;
+                    btn.textContent = "Visit"
                 }
-                // img.setAttribute("src", item.imageUrl)
-                img.setAttribute("width", "500px")
-                title.textContent = item.name;
-                // paragraph.textContent = item.description;
-                btn.textContent = "Visit"
-
+                    
                 card.append(img, title, paragraph, btn);
-
-                document.getElementById("results").appendChild(card);
+                resultsDiv.style.visibility = "visible";
+                resultsDiv.appendChild(card);
             })
 
-
             document.getElementById("searchQuery").focus();
-            // generateCard();
             resetSearch();
         })
         .catch(error => {
@@ -67,19 +67,9 @@ function fetchRecommendations(term) {
 
 function resetSearch() {
     document.getElementById("searchQuery").value = "";
-    // document.getElementById("results").innerHTML = "";
 }
 
-function generateCard() {
-    const card = document.createElement("div");
-    const img = document.createElement("img")
-    const title = document.createElement("h2");
-    const paragraph = document.createElement("p");
-    const btn = document.createElement("button");
-
-    card.setAttribute("class", "destinationCard");
-    btn.textContent = "Visit";
-    card.append(img, title, paragraph, btn);
-
-    document.getElementById("results").appendChild(card);
+function clearDestinations(){
+    resultsDiv.innerHTML = "";
+    resultsDiv.style.visibility = "hidden";
 }
